ce49cd6e629f23ea280f43e59af4b62df4e81dbb
mail2naresh@gmail.com <mail2naresh@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-02-21 02:52:52 +0000

    Adding script to cluster text.
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@9 e00e4afb-af5a-4ce0-248b-1784ae5f6371

---
+++ assignment1/text_cluster_objects.m
@@ -0,0 +1,46 @@
+%% This Script run clustering algorithm on text vectors.
+
+%% Load bag and shoes text vectors.
+load('bag_text_vector.mat')
+load('shoes_text_vector.mat')
+
+%% Create a consolidate text and filename vector.
+text_vector = [];
+filenames = {};
+
+%% For bags.
+for r = 1:size(bag_text_vector, 2)
+  text_vector(r,:) = bag_text_vector(r).vector;
+  filenames{r} = bag_text_vector(r).name; 
+end
+
+%% For Shoes.
+start = size(text_vector,1);
+for r = 1:size(shoes_text_vector, 2)
+  text_vector(r+start,:) = shoes_text_vector(r).vector;
+  filenames{r+start} = shoes_text_vector(r).name; 
+end
+
+% Create Clusters and visualize using montage.
+for numClusters = 2:2:8
+  H = [];
+  opts = statset('Display','final');
+  [cidx, ctrs] = kmeans(text_vector, numClusters, 'Distance','cosine', 'Replicates', 5, 'Options', opts);
+  handle = figure;
+  for idx = 1:numClusters
+    distances = [];
+    k = 1;
+    for fidx = 1:size(filenames,2)
+      if cidx(fidx) ==  idx
+        distances(k) = pdist2(ctrs(idx,:), gist(fidx,:));
+        k = k + 1;
+      end
+    end
+    [values, indices] = sort(distances);
+    files = {filenames{cidx==idx}};
+    files = files(indices(1:k-1));
+    montage(files(1:10:size(files,2)));
+    %fname = sprintf('output/%d_%d.jpg', numClusters, idx); 
+    %print(handle, '-djpeg', fname);
+  end
+end
