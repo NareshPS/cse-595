4c059cf0d8e18a5298c64e7760e29ca3d0af299d
mail2naresh@gmail.com <mail2naresh@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-02-21 03:16:44 +0000

    Fixing buildLexicon.
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@10 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- assignment1/buildLexicon.m
+++ assignment1/buildLexicon.m
@@ -1,14 +1,15 @@
 % build a lexicon of all words in the corpus
 % returns a cell array of all words in all the files in the given path
-function lexicon = buildLexicon(path, extension)
+function lexicon = buildLexicon(path, filePrefix, fileSuffix)
 
 if nargin == 1
-    extension = 'txt';
+    filePrefix = 'descr_';
+    fileSuffix = 'txt';
 end
 
 lexicon = {};
 
-files = dir(strcat(path, strcat('/*.',extension)));
+files = dir(fullfile(path, [filePrefix '*.' fileSuffix]));
 
 for i = 1 : numel(files)
     fid = fopen(strcat(strcat(path, '/'), files(i).name));
