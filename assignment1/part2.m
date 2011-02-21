a957d17d29b4644439fdb5f4214933fb53517407
mail2naresh@gmail.com <mail2naresh@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-02-21 00:31:36 +0000

    checking
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@6 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- assignment1/part2.m
+++ assignment1/part2.m
@@ -1,6 +1,6 @@
 %% Call buildLexicon to get the list of Lexicons.
 bagLex = buildLexicon('./bags/');
-shoesLex = buildLexicon('/shoes/');
+shoesLex = buildLexicon('./shoes/');
 
 bagDir = './bags/';
 shoesDir = './shoes/';
@@ -16,14 +16,42 @@ bag_text_vector = [];
 for idx = 1 : length(bagFiles)
     fd = fopen(fullfile(bagDir, bagFiles(idx).name));
     toks = split(fd);
-    fclose(fid);
+    fclose(fd);
+    
+    file_vector(length(bagLex)) = 0;
+    for i=1:length(toks)
+        indices = strmatch(toks(i), bagLex);
+        
+        for j = indices
+            file_vector(j) = file_vector(j) + 1;
+        end
+    end
+   
+    bag_text_vector(idx).name = fullfile(bagDir, bagFiles(idx).name);
+    bag_text_vector(idx).vector = file_vector;
 end
 
 %% Compute the frequency of each lexicon for shoes.
 
 shoes_text_vector = [];
+
 for idx = 1 : length(shoesFiles)
     fd = fopen(fullfile(shoesDir, shoesFiles(idx).name));
     toks = split(fd);
-    fclose(fid);
+    fclose(fd);
+    
+    file_vector(length(shoesLex)) = 0;
+    for i=1:length(toks)
+        indices = strmatch(toks(i), shoesLex);
+        
+        for j = indices
+            file_vector(j) = file_vector(j) + 1;
+        end
+    end
+   
+    shoes_text_vector(idx).name = fullfile(shoesDir, shoesFiles(idx).name);
+    shoes_text_vector(idx).vector = file_vector;
 end
+
+save('bag_text_vector.mat', 'bag_text_vector');
+save('shoes_text_vector.mat', 'shoes_text_vector');
