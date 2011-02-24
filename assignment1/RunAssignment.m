75d74c38ccb08b7ff428645e2fe29c0c6fbb99bc
mail2naresh@gmail.com <mail2naresh@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-02-24 05:10:19 +0000

    Adding code to create output directories
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@24 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- assignment1/RunAssignment.m
+++ assignment1/RunAssignment.m
@@ -1,3 +1,38 @@
+%% Create output directories for Part 1
+if exist('gist_shoes_out', 'dir') == 0
+    mkdir gist_shoes_out
+end
+
+if exist('gist_bag_out', 'dir') == 0
+    mkdir gist_bag_out
+end
+
+if exist('gist_all_out', 'dir') == 0
+    mkdir gist_all_out
+end
+
+%% Create output directories for Part 2
+if exist('text_shoes_out', 'dir') == 0
+    mkdir text_shoes_out
+end
+
+if exist('text_bag_out', 'dir') == 0
+    mkdir text_bag_out
+end
+
+if exist('text_all_out', 'dir') == 0
+    mkdir text_all_out
+end
+
+%% Create output directories for Part 3
+if exist('merged_shoes_out', 'dir') == 0
+    mkdir merged_shoes_out
+end
+
+if exist('merged_bag_out', 'dir') == 0
+    mkdir merged_bag_out
+end
+
 %% Run Part 1
 part1
 
