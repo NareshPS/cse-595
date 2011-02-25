45c3a35741a77c6f5e4fed466fc87722e87b942c
rohithmenon@gmail.com <rohithmenon@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-02-25 03:42:47 +0000

    Added a check to ensure atleast one image in a cluster.
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@32 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- assignment1/montageGrid.m
+++ assignment1/montageGrid.m
@@ -13,24 +13,26 @@ end
 
 map = [];
 imIndex = 1;
-imgSize = size(imread(char(filenames(1))));
-
-for i=1:rowSize
-    tempMap = [];
-    for j=1:colSize
-        if (imIndex <= numImages)
-            image = imread(char(filenames(imIndex)));
-            if size(image, 3) == 3
-                tempMap = [tempMap rgb2gray(image)];
-            else
-                tempMap = [tempMap image];
-            end
-            imIndex  = imIndex + 1;
-        else
-            tempMap = [tempMap zeros(imgSize(1), imgSize(2))];
-        end
-    end
-    map = [map; tempMap];
+if size(filenames, 2) > 0
+  imgSize = size(imread(char(filenames(1))));
+  
+  for i=1:rowSize
+      tempMap = [];
+      for j=1:colSize
+          if (imIndex <= numImages)
+              image = imread(char(filenames(imIndex)));
+              if size(image, 3) == 3
+                  tempMap = [tempMap rgb2gray(image)];
+              else
+                  tempMap = [tempMap image];
+              end
+              imIndex  = imIndex + 1;
+          else
+              tempMap = [tempMap zeros(imgSize(1), imgSize(2))];
+          end
+      end
+      map = [map; tempMap];
+  end
+  
+  montage(map);
 end
-
-montage(map);
