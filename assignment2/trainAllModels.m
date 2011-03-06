7c314ce456a0ca7b6fbd5819939103eadfbdc545
mail2naresh@gmail.com <mail2naresh@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-03-06 22:52:51 +0000

    computed probabilities table
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@51 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- assignment2/trainAllModels.m
+++ assignment2/trainAllModels.m
@@ -1,4 +1,5 @@
-% Create training and test set
+%% Create training and test set
+
 if exist('corpus.mat', 'file') == 0
     [training, test] = createCorpus('./bags');
     save('corpus.mat', 'training', 'test');
@@ -21,21 +22,39 @@ else
     load('testHist.mat');
 end
 
-% Sort training Label
-trainingLabel = [training.label];
+%% Build Models.
+if exist('model.mat', 'file') == 0
+    % Sort training Label
+    trainingLabel = [training.label];
 
-numSets = 5;
-splitSetlabels = zeros(1, size(trainingLabel, 2));
+    numSets = 5;
+    splitSetlabels = zeros(1, size(trainingLabel, 2));
 
-for i = 1:numSets;
-    for j = 1:size(trainingLabel, 2);
-        if trainingLabel(j) == i;
-            splitSetLabels(j) = 1;
-        else
-            splitSetLabels(j) = 2;
+    for i = 1:numSets;
+        for j = 1:size(trainingLabel, 2);
+            if trainingLabel(j) == i;
+                splitSetLabels(j) = 1;
+            else
+                splitSetLabels(j) = 2;
+            end
         end
+        modelArray{i} = trainModel(splitSetLabels, trainHistograms, i);
     end
-    modelArray{i} = trainModel(splitSetLabels, trainHistograms, i);
+    save('model.mat', 'modelArray');
+else
+    load('model.mat')
 end
+%% Classify testing images.
 
-save('model.mat', 'modelArray');
+if exist('allProbabilities.mat') == 0
+    probEstimates = {};
+    for i = 1:size(modelArray,2);
+        randomLabels = randsrc(1, size(testHistograms, 1), [1 2]);
+        [predictedLabels, accuracy, probEstimates{i}] = svmpredict(randomLabels', testHistograms, modelArray{i}, '-b 1');
+    end
+
+    allProbabilities =[probEstimates{1}(:,1) probEstimates{2}(:,1) probEstimates{2}(:,1) probEstimates{4}(:,1) probEstimates{5}(:,1)];
+    save('allProbabilities.mat', 'allProbabilities');
+else
+    load('allProbabilities.mat');
+end
