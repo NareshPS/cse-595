361cc8fabf6c1510cb32a12d787837a02ab06cfa
mail2naresh@gmail.com <mail2naresh@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-03-06 21:35:52 +0000

    Adding Probability Parameters
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@50 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- assignment2/trainModel.m
+++ assignment2/trainModel.m
@@ -13,9 +13,9 @@ tuningLabel = [trainLabel((trainingSize + 1):size(trainLabel, 2))]';
 tuningHistogram = trainHistograms((trainingSize + 1):size(trainLabel, 2),:);
 
 % Train SVM classifier.
-model = svmtrain(trainingLabel', trainingHistogram, '-c .1 -t 2 -g .1');
+model = svmtrain(trainingLabel', trainingHistogram, '-c .1 -t 2 -g .1 -b 1');
 
 % Test SVM classifier.
 [predictedLabels, accuracy, probEstimates] = ...
     svmpredict(tuningLabel, ...
-    tuningHistogram, model);
+    tuningHistogram, model, '-b 1');
