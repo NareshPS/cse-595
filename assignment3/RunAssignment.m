b2f6e515b16c25a24c079fb5ace6db425e866d2d
mail2naresh@gmail.com <mail2naresh@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-03-26 07:30:27 +0000

    Fixing Clustering Issue. Adding data files.
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@74 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- assignment3/RunAssignment.m
+++ assignment3/RunAssignment.m
@@ -25,24 +25,35 @@ else
 end
 
 %% Calculate sift vectors
-allSifts = [];
-allVectorMaps = [];
-imageNameIdx = 1;
 
-for catIdx = 1 : numCats
-    catTrainingSet = training{catIdx}';
-    [siftVector, vectorImageMap, imageNameIdx] = calculateSift(catTrainingSet, imageNameIdx);
-    allSifts = [allSifts; siftVector];
-    allVectorMaps = [allVectorMaps; vectorImageMap];
+if exist('siftVector.mat', 'file') == 0
+    allSifts = [];
+    allVectorMaps = [];
+    imageNameIdx = 1;
+
+    for catIdx = 1 : numCats
+        catTrainingSet = training{catIdx}';
+        [siftVector, vectorImageMap, imageNameIdx] = calculateSift(catTrainingSet, imageNameIdx);
+        allSifts = [allSifts; siftVector];
+        allVectorMaps = [allVectorMaps; vectorImageMap];
+    end
+    save('siftVector.mat', 'allSifts', 'allVectorMaps', 'imageNameIdx');
+else
+    load('siftVector.mat');
 end
-
 %% Create visual words by kmeans clustering and create the feature vector
-k = 50;
-[clusterIds, centroids] = kmeans(double(allSifts), k);
-imageFeatureVector = zeros(imageNameIdx-1, size(centroids, 1));
+if exist('siftFeatureVector.mat', 'file') == 0
+    k = 9;
+    opts = statset('MaxIter', 500, 'Display', 'iter');
+    [clusterIds, centroids] = kmeans(double(allSifts), k, 'Options', opts);
+    imageFeatureVector = zeros(imageNameIdx-1, size(centroids, 1));
 
-for siftIdx = 1 : size(clusterIds, 1)
-    imageIdx = allVectorMaps(siftIdx);
-    imageFeatureVector(imageIdx, clusterIds(siftIdx)) = ...
-        imageFeatureVector(imageIdx, clusterIds(siftIdx)) + 1;
+    for siftIdx = 1 : size(clusterIds, 1)
+        imageIdx = allVectorMaps(siftIdx);
+        imageFeatureVector(imageIdx, clusterIds(siftIdx)) = ...
+            imageFeatureVector(imageIdx, clusterIds(siftIdx)) + 1;
+    end
+    save('siftFeatureVector.mat', 'imageFeatureVector');
+else
+    load('siftFeatureVector.mat');
 end
