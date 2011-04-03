1d0ea5dd4977d1a91a678a75da69cb442359c35a
mail2naresh@gmail.com <mail2naresh@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-04-03 23:40:49 +0000

    Automatically creating directories.
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@91 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- project/run_mr.sh
+++ project/run_mr.sh
@@ -5,9 +5,11 @@ INPUT_LIST=$1
 INPUT_FILENAME=`basename $INPUT_LIST`
 
 hadoop fs -rm "${HPATH}/gistin/$INPUT_FILENAME" 
+hadoop fs -mkdir "${HPATH}/gistin"
 hadoop fs -copyFromLocal $INPUT_LIST "${HPATH}/gistin/$INPUT_FILENAME"
 
 hadoop fs -rm "${HPATH}/libraries/libgist.so"
+hadoop fs -mkdir "${HPATH}/libraries"
 make
 hadoop fs -copyFromLocal libgist.so "${HPATH}/libraries/libgist.so" 
 
@@ -16,4 +18,4 @@ hadoop fs -copyFromLocal libgist.so "${HPATH}/libraries/libgist.so"
 hadoop fs -rmr "${HPATH}/gistout"
 time hadoop jar GistCalculator.jar GistCalculatorMR "${HPATH}/gistin/$INPUT_FILENAME" wap/gistout "${HPATH}/libraries/libgist.so#libgist.so"
 rm -rf gistout
-hadoop fs -get /user/rohith/wap/gistout gistout
+hadoop fs -get $HPATH/gistout gistout
