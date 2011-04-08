a3f04557f5a9992a11cf9c93914424fffb18b8dd
rohithmenon@gmail.com <rohithmenon@gmail.com@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-04-08 04:48:42 +0000

    Fixed the run_mr for gist feature calculation.
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@99 e00e4afb-af5a-4ce0-248b-1784ae5f6371

--- project/run_mr.sh
+++ project/run_mr.sh
@@ -1,12 +1,12 @@
 USERNAME=`whoami`
 HPATH=/user/$USERNAME/wap
 
-INPUT_LIST=$1
-INPUT_FILENAME=`basename $INPUT_LIST`
+#INPUT_LIST=$1
+#INPUT_FILENAME=`basename $INPUT_LIST`
 
-hadoop fs -rm "${HPATH}/gistin/$INPUT_FILENAME" 
-hadoop fs -mkdir "${HPATH}/gistin"
-hadoop fs -copyFromLocal $INPUT_LIST "${HPATH}/gistin/$INPUT_FILENAME"
+#hadoop fs -rm "${HPATH}/gistin/$INPUT_FILENAME" 
+#hadoop fs -mkdir "${HPATH}/gistin"
+#hadoop fs -copyFromLocal $INPUT_LIST "${HPATH}/gistin/$INPUT_FILENAME"
 
 hadoop fs -rm "${HPATH}/libraries/libgist.so"
 hadoop fs -mkdir "${HPATH}/libraries"
@@ -16,6 +16,6 @@ hadoop fs -copyFromLocal libgist.so "${HPATH}/libraries/libgist.so"
 ./build.sh
 
 hadoop fs -rmr "${HPATH}/gistout"
-time hadoop jar GistCalculator.jar GistCalculatorMR "${HPATH}/gistin/$INPUT_FILENAME" wap/gistout "${HPATH}/libraries/libgist.so#libgist.so"
+time hadoop jar GistCalculator.jar GistCalculatorMR "${HPATH}/gistin/x*" wap/gistout "${HPATH}/libraries/libgist.so#libgist.so"
 rm -rf gistout
 hadoop fs -get $HPATH/gistout gistout
