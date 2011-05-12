4e9205c398d81f296713df68080b8bdacad19d06
sandesh247 <sandesh247@e00e4afb-af5a-4ce0-248b-1784ae5f6371>
2011-05-12 03:12:58 +0000

    sift evaluations
    git-svn-id: https://cse-595.googlecode.com/svn/trunk@129 e00e4afb-af5a-4ce0-248b-1784ae5f6371

---
+++ project/GistPredictor/src/SiftTagPredictor.java
@@ -0,0 +1,104 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+
+import weka.classifiers.Classifier;
+import weka.core.SparseInstance;
+
+public class SiftTagPredictor {
+	private static final int TagPredictThreshold = 20;
+
+	public static void main(String[] args) throws Exception {
+
+		TagModel tagModel = new TagModel(args[0]);
+
+		Classifier cls = (Classifier) weka.core.SerializationHelper
+				.read(args[1]);
+
+		List<String> allTags = new ArrayList<String>();
+		Scanner tagsIn = new Scanner(new FileReader(args[2]));
+		while (tagsIn.hasNextLine())
+			allTags.add(tagsIn.nextLine());
+
+		Scanner in = new Scanner(System.in);
+
+		while (in.hasNextLine()) {
+			Gist gist = Gist.parseGistFromString(in.nextLine());
+			
+			Set<String> originalTags = new HashSet<String>();
+			for (String originalTag : gist.getLabel().split("|")) {
+				originalTags.add(originalTag);
+			}
+			
+			// get twice the number of tags we want to predict
+			Set<WordScore> tagScores = tagModel.getBestKTags(gist.getLabel(),
+					TagPredictThreshold * 2);
+
+			SparseInstance testInstance = new SparseInstance(
+					Gist.getGistLength() + 1);
+
+			int idx = 1;
+			List<Double> gistValues = gist.getGistValues();
+			for (Double gistValue : gistValues) {
+				testInstance.setValue(idx++, gistValue);
+			}
+
+			testInstance.setValue(0, "");
+			double[] probabilities = cls.distributionForInstance(testInstance);
+
+			List<WordScore> testScores = new ArrayList<WordScore>();
+			for (int i = 0; i < allTags.size(); ++i) {
+				testScores.add(new WordScore(allTags.get(i), probabilities[i]));
+			}
+
+			Collections.sort(testScores);
+
+			List<WordScore> intersection = new ArrayList<WordScore>();
+			int numPredicted = 0;
+			for (WordScore classifiedScore : testScores) {
+				for (WordScore modelScore : tagScores) {
+					if (classifiedScore.word.equals(modelScore.word)) {
+						++numPredicted;
+						intersection.add(new WordScore(modelScore.word, 0d));
+					}
+				}
+
+				if (numPredicted >= TagPredictThreshold)
+					break;
+			}
+			
+			System.out.println(gist.getFileId());
+			
+			WordScore[][] allScores = new WordScore[][] {
+					tagScores.toArray(new WordScore[] {}),
+					testScores.toArray(new WordScore[] {}),
+					intersection.toArray(new WordScore[] {})
+			};
+			for (WordScore[] scores : allScores) {
+				System.out.println(StringUtils.join(ArrayUtils.subarray(scores, 0, TagPredictThreshold), ","));
+			}
+			
+			for (WordScore[] scores : allScores) {
+				int count = 0;
+				for(int i = 0; i < 5; ++i) {
+					if(originalTags.contains(scores[i].word)) {
+						count += 1;
+					}
+				}
+				
+				System.out.print(count + " ");
+			}
+			
+			System.out.println();
+			System.out.println();
+		}
+	}
+}
